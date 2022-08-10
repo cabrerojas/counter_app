1ff016dd1b80e05201b78af726f91a7913ad813a
Guillermo Cabrera Rojas <gcabrerojas@gmail.com>
2022-08-10 15:39:48 -0400

    final counter app

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -2,13 +2,42 @@ import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
+  
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-        backgroundColor: Colors.indigoAccent,
-        body: Center(
-          child: Text('Home Screen'),
-        ));
+    const fontSize25 = TextStyle(fontSize: 25);
+    int counter = 10;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('HomeScreen'),
+        backgroundColor: Colors.amber,
+        elevation: 5.2,
+      ),
+      backgroundColor: Colors.amberAccent,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            const Text('Cantidad de clics', style: fontSize25),
+            Text('$counter', style: fontSize25),
+          ],
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.amber,
+        child: const Icon(
+          Icons.add_circle_outline,
+          size: 30.0,
+        ),
+        onPressed: () {
+          print('funciona: $counter');
+          counter++;
+        },
+      ),
+    );
   }
 }
